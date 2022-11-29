4e771eb76a7442bf72de14799c52f8449573bda9
byeong cheol lim <lim6112j@gmail.com>
2022-11-29 16:01:39 +0900

    generic iterator

--- src/bin/type-driven.rs
+++ src/bin/type-driven.rs
@@ -1,8 +1,11 @@
 use std::{thread::sleep, time::Duration};
 const CLEAR: &str = "\x1B[2J\x1B[1;1H";
-fn progress<T>(v: Vec<T>, f: fn(&T) -> ()) {
+trait Progress {
+
+}
+fn progress<T, Aiter>(iter: Aiter, f: fn(Aiter::Item) -> ()) where Aiter: Iterator<Item = T> {
     let mut i = 1;
-    for n in v.iter() {
+    for n in iter {
         println!("{}{}", CLEAR, "*".repeat(i));
         i += 1;
         f(n);
@@ -13,5 +16,5 @@ fn expensive_calculation(_n: &i32) {
 }
 fn main() {
     let v = vec![1, 2, 3];
-    progress(v, expensive_calculation);
+    progress(v.iter(), expensive_calculation);
 }
