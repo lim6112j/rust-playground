82eb69eafea2c44ca6bf438f805994a6989400e7
lim6112j <lim6112j@gmail.com>
2022-11-30 00:41:44 +0900

    custom box pointer

--- src/bin/deref.rs
+++ src/bin/deref.rs
@@ -1,6 +1,20 @@
+// [confer](https://doc.rust-lang.org/book/ch15-02-deref.html)
+use std::ops::Deref;
+struct MyBox<T>(T);
+impl<T> MyBox<T> {
+    fn new (x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
 fn main() {
     let x = 5;
-    let y = &x;
+    let y = MyBox::new(x);
     assert_eq!(5, x);
     assert_eq!(5, *y);
 }
