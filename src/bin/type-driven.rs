822f1eb457f65ce6f634deb0a34024796def9f7c
byeong cheol lim <lim6112j@gmail.com>
2022-11-29 16:40:42 +0900

    iterator trait extended

--- src/bin/type-driven.rs
+++ src/bin/type-driven.rs
@@ -1,20 +1,29 @@
 use std::{thread::sleep, time::Duration};
 const CLEAR: &str = "\x1B[2J\x1B[1;1H";
-trait Progress {
-
+struct Progress<Aiter> {
+    iter: Aiter,
+    i: usize
 }
-fn progress<T, Aiter>(iter: Aiter, f: fn(Aiter::Item) -> ()) where Aiter: Iterator<Item = T> {
-    let mut i = 1;
-    for n in iter {
-        println!("{}{}", CLEAR, "*".repeat(i));
-        i += 1;
-        f(n);
+impl<Aiter> Progress< Aiter> {
+    pub fn new(iter: Aiter) -> Self {
+        Progress {iter, i: 0}
     }
 }
+impl<Aiter> Iterator for Progress<Aiter> where Aiter: Iterator{
+    type Item = Aiter::Item;
+    fn next(&mut self) -> Option<Self::Item> {
+        println!("{}{}", CLEAR, "*".repeat(self.i));
+        self.i += 1;
+        self.iter.next()
+    }
+}
+
 fn expensive_calculation(_n: &i32) {
     sleep(Duration::from_secs(1));
 }
 fn main() {
     let v = vec![1, 2, 3];
-    progress(v.iter(), expensive_calculation);
+    for n in Progress::new(v.iter()) {
+        expensive_calculation(n);
+    }
 }
